0b3d2e998ade60f1620945e216d8fa395cdafc79
eoood <54926197+eoood@users.noreply.github.com>
2019-11-14 17:22:03 +0900

    web
    web

--- web/module/DB.py
+++ web/module/DB.py
@@ -2,8 +2,8 @@ import pymysql
 import pandas as pd
 class Db():
     def __init__(self):
-        self.conn = pymysql.connect( host='localhost', user='root', password='1234', charset='utf8', db='factory')
-        self.cursor=conn.cursor(pymysql.cursors.DictCursor)
+        self.conn = pymysql.connect( host='192.168.0.2', user='jkh', password='1234', charset='utf8', db='factory')
+        self.cursor=self.conn.cursor(pymysql.cursors.DictCursor)
     
     def read_data_dataframe(self,qurey):
         return pd.read_sql_query(qurey,self.conn)
