25b4e44368e9b059a75f0b35fcf59974000e38e9
jkh5178 <jkh5178@naver.com>
2019-11-15 15:18:52 +0900

    jkh
    그래프 그리기를 위한 작업
    websocket을 사용하여 push

--- web/app.py
+++ web/app.py
@@ -1,10 +1,33 @@
-from flask import Flask, url_for, render_template, request, redirect
+from flask import Flask, url_for, jsonify,render_template, request, redirect
+from subprocess import call
+from flask_socketio import SocketIO, send
+from module.DB import Db
+import time
+from threading import Thread
+db=Db()
 
 app = Flask( __name__ )
+app.secret_key = "mysecret"
+socket_io = SocketIO(app)
 
+def push_data():
+    global socket_io
+    global db
+    while True :
+        sql="select count(*) as num, date from log group by date"
+        data=db.read_data_dataframe(sql)
+        send_data=data.to_dict()
+        print(send_data)
+        socket_io.emit('new',send_data,broadcast=True)
+        time.sleep(10)
+
+push_data_thread=Thread(target=push_data)
 
 @app.route("/")
 def main():
+    global push_data_thread
+    if not push_data_thread.isAlive():
+        push_data_thread.start()
     return render_template('main.html')
 
 @app.route( "/management", methods = [ 'GET', 'POST' ] )
@@ -12,5 +35,7 @@ def modify():
     if request.method == 'GET':
         return render_template( "management.html" )
 
+
+
 if __name__ == "__main__":
-    app.run( host = "0.0.0.0", port = 8080, debug = True )
+    socket_io.run( app=app, port = 8080, debug = True )
--- web/module/DB.py
+++ web/module/DB.py
@@ -3,7 +3,7 @@ import pandas as pd
 class Db():
     def __init__(self):
         self.conn = pymysql.connect( host='localhost', user='root', password='1234', charset='utf8', db='factory')
-        self.cursor=conn.cursor(pymysql.cursors.DictCursor)
+        self.cursor=self.conn.cursor(pymysql.cursors.DictCursor)
     
     def read_data_dataframe(self,qurey):
         return pd.read_sql_query(qurey,self.conn)
