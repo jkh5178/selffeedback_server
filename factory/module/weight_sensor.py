2032deef3de37ded58dfc63eb59e189681ae0eb0
jkh5178 <jkh5178@naver.com>
2019-11-16 17:36:05 +0900

    jkh
    client update 부분 수정

--- factory/module/weight_sensor.py
+++ factory/module/weight_sensor.py
@@ -1,12 +1,14 @@
 from module.Thread_client import Client
 from module.factory_enum import device
 class WeightSeneor(Client):
+    
     def __init__(self,index,client,mainThread,target_weight,target_range):
         Client.__init__(self,index,client,mainThread)
         self.send_to_device(str(target_weight)+'\n')
         self.send_to_device(str(target_range)+'\n')
         self.target_weight=target_weight
         self.target_range=target_range*0.01
+        self.result_message=""
     def do_job(self):
         while True:
                 self.content=self.client.recv(32)
@@ -15,19 +17,28 @@ class WeightSeneor(Client):
                     self.send_to_thread(device.CONVEYER,message+'\n')
                     #그외에는 무게 측정 값이 전달 됨으로 판별후 DB에 저장
                     print(self.tm.count)
-                    if (self.tm.count%10==0):
+                    if(self.tm.count>10 and self.tm.count%2==0):
                         self.tm.lean_thread()
-                else:
+
+                    elif (self.tm.count%2==0 and self.tm.count<=10):
+                        self.tm.opentime-=200
+                        self.tm.db.update_set_value(self.tm.opentime)
+                        self.tm.refrash_data()
                     
+
+                else:
                     self.weight=float(message)
                     if(self.check_weight(weight=self.weight)):
+                        result_message="true"
                         print(self.weight,"ture")
                         self.tm.savedata(self.weight,1)
                     else:
-                        print(self.weight,"flase")
+                        result_message="false"
+                        print(self.weight,"false")
                         self.tm.savedata(self.weight,0)
+                    self.send_to_device(result_message)
                     self.tm.count+=1
-                
+
     def check_weight(self, weight):
         if(weight>=self.target_weight-self.target_weight*self.target_range and weight<=self.target_weight+self.target_weight*self.target_range):
             return True
